db87dda3e38e02238eb76bbc6ced1fa35fb6fd03
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2022-10-04 18:53:57 +0200

    little update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,8 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+
+CheckNumbers() {
+throw new Error('Method not implemented.');
+}
+resetPage() {
+throw new Error('Method not implemented.');
+}
   min = 1;
   max = 50;
+numeroIntroducido: any;
+pista: any;
   constructor() {}
   
   
